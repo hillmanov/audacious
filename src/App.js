044b8848bfe5a73f25e098434aa4ee21ff33a717
Scott Hillman <hillmanov@gmail.com>
2020-05-19 06:18:50 -0600

    File handling, saving, restoring, and playback rate
    Working experiments:
    1) Importing MP3 files
    2) Persisting those with localForage in the context of an augmented MST snapshot (not a true snapshot, as the files have to be in volatile)
    3) Restoring files and playback rate
    4) Live playback rate adjustments
    Audio playback works, as does restoring from localforage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,108 @@
 import React from 'react';
-import logo from './logo.svg';
+import { map } from 'lodash';
+import { reaction, when } from 'mobx';
+import { observer } from 'mobx-react';
+import { types, applySnapshot, onSnapshot, getSnapshot } from 'mobx-state-tree';
+import * as localForage from 'localforage';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const { model } = types;
+
+const Controller = model('AppController', {
+  playbackRate: 1,
+})
+.volatile(() => ({
+  songs: [],
+  audioElement: null,
+}))
+.actions(self => ({
+  afterCreate() {
+    self.init();
+  },
+  async init() {
+
+    const state = await localForage.getItem('state');
+    applySnapshot(self, state);
+
+    if (state.songs) {
+      self.setSongs(state.songs);
+    }
+
+    reaction(
+      () => [getSnapshot(self), self.songs],
+      ([snapshot, songs]) => localForage.setItem('state', { ...snapshot, songs })
+    );
+
+    when(
+      () => self.audioElement,
+      () => {
+        self.setPlaybackRate(self.playbackRate);
+      }
+    );
+  },
+  setSongs(songs) {
+    self.songs = songs;
+  },
+  setAudioElement(audioElement) {
+    self.audioElement = audioElement;
+  },
+  playSong(song) {
+    const fileSrc = URL.createObjectURL(song);
+    self.audioElement.src = fileSrc;
+    self.audioElement.play();
+    self.setPlaybackRate(self.playbackRate);
+    window.document.title = song.name.replace('.mp3', '');
+  },
+  setPlaybackRate(playbackRate) {
+    self.playbackRate = playbackRate;
+    self.audioElement.playbackRate = playbackRate;
+  },
+  adjustPlaybackRate(adjustment) {
+    self.setPlaybackRate(self.playbackRate + adjustment)
+  }
+}))
+
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.c = Controller.create()
+    this.audioElement = React.createRef();
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          id="song-input"
+          onChange={(e) => this.c.setSongs(e.currentTarget.files)}
+          type="file"
+          multiple
+          accept="audio/mp3"
+        />
+        {map(this.c.songs, song => (
+          <div key={song.name} onClick={() => this.c.playSong(song)}>{song.name}</div>
+        ))}
+
+        <audio
+          controls
+          ref={(e) => this.c.setAudioElement(e)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <track />
+        </audio>
+        <div>
+          {this.c.playbackRate}
+        </div>
+        <button onClick={() => this.c.setPlaybackRate(2)}>2</button>
+        <button onClick={() => this.c.setPlaybackRate(1.5)}>1.5</button>
+        <button onClick={() => this.c.setPlaybackRate(1)}>1</button>
+
+        <button onClick={() => this.c.adjustPlaybackRate(.1)}>+.1</button>
+        <button onClick={() => this.c.adjustPlaybackRate(.01)}>+.01</button>
+        <button onClick={() => this.c.adjustPlaybackRate(-.1)}>-.1</button>
+        <button onClick={() => this.c.adjustPlaybackRate(-.01)}>-.01</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default observer(App);
